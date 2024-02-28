d6c9c1670c1df6a0c9ca1052de6abb88de088916
adith-p <adithprakash008@gmail.com>
2024-02-28 15:24:27 +0530

    Added DB

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-@xt=py&2987l26(e!i5fr*_*@w83e%m8bgr=&t1sn8_69o9tb5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app',]
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 
 
 # Application definition
